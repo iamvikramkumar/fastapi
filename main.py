16addb3f81d753b690f0eb4e7e3a35d5031b3dd0
iamvikramkumar <iamvikramkumar5@gmail.com>
2025-06-29 01:08:16 +0530

    feat: create basic FastAPI endpoint

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pydantic import BaseModel   # Base model help for validation 
+from typing import List
+
+
+app = FastAPI()
+
+class Tea(BaseModel):
+    id: int
+    name: str
+    origin: str
+
+teas: List[Tea] = []
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Chai Code"}
+
+@app.get("/teas")
+def get_teas():
+    return teas
+
+@app.post("/teas")
+def add_tea(tea: Tea):
+    teas.append(tea)
+    return tea
+
+@app.put("/teas/{tea_id}")
+def update_tea(tea_id: int, updated_tea: Tea):
+    for index, tea in enumerate(teas):
+        if tea.id == tea_id:
+            teas[index] = update_tea
+            return tea
+    return {"error": "Tea not found"}
+
+@app.delete("/teas/{tea_id}")
+def delete_tea(tea_id: int):
+    for index, tea in enumerate(teas):
+        if tea.id == tea_id:
+            deleted_tea = teas.pop(index)
+            return deleted_tea
+    return {"error": "Tea not found"}
